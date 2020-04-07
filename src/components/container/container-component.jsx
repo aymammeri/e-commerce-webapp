36914f1ad74a1023da6f77ca96b033aae8c83955
Ayoub Mammeri <ay.mammeri@gmail.com>
2020-04-07 15:31:05 -0400

    add sass, structure and organize project directory (add homepage components and container)

---
+++ src/components/container/container-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item-component';
+
+const Container = () => {
+  return (
+    <div>
+      <MenuItem />
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/pages/homepage/homepage-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../../components/container/container-component';
+
+const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <Container />
+    </div>
+  );
+};
+
+export default HomePage;
